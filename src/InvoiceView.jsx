8794b0182578029b694ad8d25e3c96db9ac8ecb6
TedJenkler <Tedjenkler@gmail.com>
2024-03-07 11:44:35 +0100

    view invoice styled

--- src/InvoiceView.jsx
+++ src/InvoiceView.jsx
@@ -12,68 +12,75 @@ function InvoiceView() {
     const totalAmount = selectedstate.items.reduce((acc, item) => acc + (item.quantity * item.price), 0);
     console.log(selectedstate)
   return (
-    <main className='px-6 pt-8 bg-lightbg h-screen'>
+    <main className='pt-8 bg-lightbg h-full'>
+        <div className='mx-6'>
         <BackBtn />
-        <div className='flex justify-between bg-white p-6 rounded-md items-center mb-4'>
+        </div>
+        <div className='flex justify-between bg-white p-6 rounded-md items-center mb-4 mx-6'>
             <p className='px13 text-coolgrey'>Status</p>
             <div className={selectedstate.status === "paid" ? "bg-paid/10 w-24 h-10 flex justify-center items-center rounded-md gap-1" : selectedstate.status === "pending" ? "bg-pending/10 w-24 h-10 flex justify-center items-center rounded-md gap-1" : selectedstate.status === "draft" ? "bg-draft/10 w-24 h-10 flex justify-center items-center rounded-md gap-1" : null}>
                 <img src={selectedstate.status === "paid" ? paid : selectedstate.status === "pending" ? pending : selectedstate.status === "draft" ? draft : null} alt={selectedstate.status === "paid" ? paid : selectedstate.status === "pending" ? pending : selectedstate === "draft" ? draft : null} />
                 <p className={selectedstate.status === "paid" ? "px15 text-paid" : selectedstate.status === "pending" ? "px15 text-pending" : selectedstate.status === "draft" ? "px15 text-draft" : null}>{selectedstate.status.charAt(0).toUpperCase() + selectedstate.status.slice(1)}</p>
             </div>
         </div>
-        <div className='p-6 bg-white'>
+        <div className='p-6 bg-white rounded-md mb-14 mx-6'>
         <div>
             <div>
-                <h2>{selectedstate.id}</h2>
-                <p>{selectedstate.description}</p>
+                <h2 className='px15 text-08 mb-1'><span className='text-07 px15'>#</span>{selectedstate.id}</h2>
+                <p className='px13 text-07 mb-8'>{selectedstate.description}</p>
             </div>
             <div>
-                <p>{selectedstate.senderAddress.street}</p>
-                <p>{selectedstate.senderAddress.city}</p>
-                <p>{selectedstate.senderAddress.postCode}</p>
-                <p>{selectedstate.senderAddress.country}</p>
+                <p className='px13 text-07 mb-1'>{selectedstate.senderAddress.street}</p>
+                <p className='px13 text-07 mb-1'>{selectedstate.senderAddress.city}</p>
+                <p className='px13 text-07 mb-1'>{selectedstate.senderAddress.postCode}</p>
+                <p className='px13 text-07 mb-8'>{selectedstate.senderAddress.country}</p>
+            </div>
+        </div>
+        <div className='flex'>
+            <div className='mb-10'>
+                <p className='px13 text-07 mb-3'>Invoice Date</p>
+                <p className='px15 text-08 mb-8'>{selectedstate.createdAt}</p>
+                <p className='px13 text-07 mb-3'>Payment Due</p>
+                <p className='px15 text-08 mb-8'>{selectedstate.paymentDue}</p>
+                <p className='px13 text-07 mb-3'>Sent to</p>
+                <p className='px15 text-08'>{selectedstate.clientEmail}</p>
+            </div>
+            <div>
+                <p className='px13 text-07 mb-3'>Bill To</p>
+                <p className='px15 text-08 mb-2'>{selectedstate.clientName}</p>
+                <p className='px13 text-07 mb-1'>{selectedstate.clientAddress.street}</p>
+                <p className='px13 text-07 mb-1'>{selectedstate.clientAddress.city}</p>
+                <p className='px13 text-07 mb-1'>{selectedstate.clientAddress.postCode}</p>
+                <p className='px13 text-07 mb-1'>{selectedstate.clientAddress.country}</p>
             </div>
         </div>
         <div>
-            <div>
-                <p>Invoice Date</p>
-                <p>{selectedstate.createdAt}</p>
-                <p>Payment Due</p>
-                <p>{selectedstate.paymentDue}</p>
-                <p>Sent to</p>
-                <p>{selectedstate.clientEmail}</p>
-            </div>
-            <div>
-                <p>Bill To</p>
-                <p>{selectedstate.clientName}</p>
-                <p>{selectedstate.clientAddress.street}</p>
-                <p>{selectedstate.clientAddress.city}</p>
-                <p>{selectedstate.clientAddress.postCode}</p>
-                <p>{selectedstate.clientAddress.country}</p>
-            </div>
-        </div>
-        <div>
-            <div>
+            <div className='bg-ghostwhite rounded-t-lg p-6 flex flex-col pb-0'>
                 {selectedstate.items.map((item) => {
                     console.log(item)
                     return (
-                        <div>
+                        <div className='flex justify-between items-center'>
                             <div>
-                                <h2>{item.name}</h2>
-                                <p>{item.quantity} x £ {item.price}</p>
+                                <h2 className='px15 text-08 mb-2'>{item.name}</h2>
+                                <p className='px5 text-07 mb-6'>{item.quantity} x £ {item.price}</p>
                             </div>
                             <div>
-                                <p>£ {item.total}</p>
+                                <p className='px15 text-08 mb-6'>£ {item.total}</p>
                             </div>
                         </div>
                     )
                 })}
             </div>
-            <div>
-                <p>Grand Total</p>
-                <p>£ {totalAmount}</p>
+            <div className='bg-charcoal rounded-b-lg p-6 flex justify-between items-center'>
+                <p className='px13 text-white'>Grand Total</p>
+                <p className='px24 text-white'>£ {totalAmount}</p>
             </div>
         </div>
+        </div>
+        <div className='bg-white w-full h-24 flex items-center justify-center gap-2 px-6'>
+            <button className='edit'>Edit</button>
+            <button className='delete'>Delete</button>
+            <button className='paid'>Mark as Paid</button>
         </div>
     </main>
   )
