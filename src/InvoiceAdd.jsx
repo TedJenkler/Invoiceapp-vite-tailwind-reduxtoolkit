1a830e8af09c9b5990b063f95b2199d60c22fc28
TedJenkler <Tedjenkler@gmail.com>
2024-03-08 16:14:48 +0100

    darklighttoggle done

--- src/InvoiceAdd.jsx
+++ src/InvoiceAdd.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import BackBtn from './components/BackBtn';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import trashcan from "./assets/trashcan.png";
 import { Link } from 'react-router-dom';
 import { add } from './features/state/stateSlice';
 
 function InvoiceAdd() {
   const dispatch = useDispatch();
+  const theme = useSelector((state) => state.state.toggleMode)
 
   const [address, setAddress] = useState();
   const [city, setCity] = useState();
@@ -75,90 +76,90 @@ const handleInputChange = (index, fieldName, value) => {
   }
 
   return (
-    <main>
+    <main className={theme === "light" ? "light2" : "dark2"}>
       <div className='pt-8 px-6 mb-6'>
         <BackBtn />
       </div>
 
       <section className='px-6 flex flex-col'>
         <p className='px15 text-01 mb-6'>Bill From</p>
-        <label className='px13 text-07 mb-2'>Street Address</label>
-        <input onChange={(e) => setAddress(e.target.value)} value={address} className='border border-05 h-12 rounded mb-6 px-5'></input>
+        <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Street Address</label>
+        <input onChange={(e) => setAddress(e.target.value)} value={address} className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-6 px-5' : 'dark4 border border-03 h-12 rounded mb-6 px-5'}></input>
         <div className='flex gap-4 justify-between mb-6'>
           <div className='flex flex-col w-1/2'>
-            <label className='px13 text-07 mb-2'>City</label>
-            <input onChange={(e) => setCity(e.target.value)} value={city} className='border border-05 w-full h-12 rounded px-5'></input>
+            <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>City</label>
+            <input onChange={(e) => setCity(e.target.value)} value={city} className={theme === "light" ? 'light4 border border-05 w-full h-12 rounded px-5' : 'dark4 border border-03 w-full h-12 rounded px-5'}></input>
           </div>
           <div className='flex flex-col w-1/2'>
-            <label className='px13 text-07 mb-2'>Post Code</label>
-            <input onChange={(e) => setPostal(e.target.value)} value={postal} className='border border-05 w-full h-12 rounded px-5'></input>
+            <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Post Code</label>
+            <input onChange={(e) => setPostal(e.target.value)} value={postal} className={theme === "light" ? 'light4 border border-05 w-full h-12 rounded px-5' : 'dark4 border border-03 w-full h-12 rounded px-5'}></input>
           </div>
         </div>
-        <label className='px13 text-07 mb-2'>Country</label>
-        <input onChange={(e) => setCountry(e.target.value)} value={country} className='border border-05 h-12 rounded mb-6 px-5'></input>
+        <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Country</label>
+        <input onChange={(e) => setCountry(e.target.value)} value={country} className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-6 px-5' : 'dark4 border border-03 h-12 rounded mb-6 px-5'}></input>
 
         <p className='px15 text-01 mb-6'>Bill To</p>
-        <label className='px13 text-07 mb-2'>Client’s Name</label>
-        <input onChange={(e) => setName(e.target.value)} value={name} className='border border-05 h-12 rounded mb-6 px-5'></input>
-        <label className='px13 text-07 mb-2'>Client’s Email</label>
-        <input onChange={(e) => setEmail(e.target.value)} value={email} className='border border-05 h-12 rounded mb-6 px-5'></input>
-        <label className='px13 text-07 mb-2'>Street Address</label>
-        <input onChange={(e) => setAddress2(e.target.value)} value={address2} className='border border-05 h-12 rounded mb-6 px-5'></input>
+        <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Client’s Name</label>
+        <input onChange={(e) => setName(e.target.value)} value={name} className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-6 px-5' : 'dark4 border border-03 h-12 rounded mb-6 px-5'}></input>
+        <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Client’s Email</label>
+        <input onChange={(e) => setEmail(e.target.value)} value={email} className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-6 px-5' : 'dark4 border border-03 h-12 rounded mb-6 px-5'}></input>
+        <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Street Address</label>
+        <input onChange={(e) => setAddress2(e.target.value)} value={address2} className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-6 px-5' : 'dark4 border border-03 h-12 rounded mb-6 px-5'}></input>
         <div className='flex gap-4 justify-between mb-6'>
           <div className='flex flex-col w-1/2'>
-            <label className='px13 text-07 mb-2'>City</label>
-            <input onChange={(e) => setCity2(e.target.value)} value={city2} className='border border-05 w-full h-12 rounded px-5'></input>
+            <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>City</label>
+            <input onChange={(e) => setCity2(e.target.value)} value={city2} className={theme === "light" ? 'light4 border border-05 w-full h-12 rounded px-5' : 'dark4 border border-03 w-full h-12 rounded px-5'}></input>
           </div>
           <div className='flex flex-col w-1/2'>
-            <label className='px13 text-07 mb-2'>Post Code</label>
-            <input onChange={(e) => setPostal2(e.target.value)} value={postal2} className='border border-05 w-full h-12 rounded px-5'></input>
+            <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Post Code</label>
+            <input onChange={(e) => setPostal2(e.target.value)} value={postal2} className={theme === "light" ? 'light4 border border-05 w-full h-12 rounded px-5' : 'dark4 border border-03 w-full h-12 rounded px-5'}></input>
           </div>
         </div>
-        <label className='px13 text-07 mb-2'>Country</label>
-        <input onChange={(e) => setCountry2(e.target.value)} value={country2} className='border border-05 h-12 rounded mb-6 px-5'></input>
+        <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Country</label>
+        <input onChange={(e) => setCountry2(e.target.value)} value={country2} className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-6 px-5' : 'dark4 border border-03 h-12 rounded mb-6 px-5'}></input>
 
-        <label className='px13 text-07 mb-2'>Invoice Date</label>
-        <input onChange={(e) => setDate(e.target.value)} value={date} className='border border-05 h-12 rounded mb-6 px-5' type='date'></input>
-        <label className='px13 text-07 mb-2'>Payment Terms</label>
-        <select onChange={(e) => setNet(parseInt(e.target.value))} value={net} className='border border-05 h-12 rounded mb-6 px-5'>
+        <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Invoice Date</label>
+        <input onChange={(e) => setDate(e.target.value)} value={date} className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-6 px-5' : 'dark4 border border-03 h-12 rounded mb-6 px-5'} type='date'></input>
+        <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Payment Terms</label>
+        <select onChange={(e) => setNet(parseInt(e.target.value))} value={net} className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-6 px-5' : 'dark4 border border-03 h-12 rounded mb-6 px-5'}>
           <option value="30">Net 30 Days</option>
           <option value="14">Net 14 Days</option>
           <option value="7">Net 7 Days</option>
           <option value="1">Net 1 Day</option>
         </select>
-        <label className='px13 text-07 mb-2'>Project Description</label>
-        <input onChange={(e) => {setDescription(e.target.value)}} value={description} className='border border-05 w-full h-12 rounded mb-20 px-5'></input>
+        <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Project Description</label>
+        <input onChange={(e) => {setDescription(e.target.value)}} value={description} className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-20 px-5' : 'dark4 border border-03 h-12 rounded mb-20 px-5'}></input>
 
         <h2 className='text-lg font-bold tracking-[-0.38px] text-lightgrey mb-6'>Item List</h2>
         {items.map((item, index) => (
           <div key={index} className='flex flex-col mb-6'>
-            <label className='px13 text-07 mb-2'>Item Name</label>
+            <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Item Name</label>
             <input
               value={item.name}
               onChange={(e) => handleInputChange(index, 'name', e.target.value)}
-              className='border border-05 h-12 rounded mb-6'
+              className={theme === "light" ? 'light4 border border-05 h-12 rounded mb-6 px-6' : 'dark4 border border-03 h-12 rounded mb-6 px-6'}
             ></input>
             <div className='flex justify-between items-center'>
               <div className='flex items-center'>
                 <div className='flex flex-col w-16 mr-4'>
-                  <label>Qty.</label>
+                  <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Qty.</label>
                   <input
                     value={item.quantity}
                     onChange={(e) => handleInputChange(index, 'quantity', e.target.value)}
-                    className='border border-05 h-12 rounded'
+                    className={theme === "light" ? 'light4 border border-05 h-12 rounded px-6' : 'dark4 border border-03 h-12 rounded px-6'}
                   ></input>
                 </div>
                 <div className='flex flex-col w-24 mr-4'>
-                  <label>Price</label>
+                  <label className={theme === "light" ? 'px13 text-07 mb-2' : 'px13 text-06 mb-2'}>Price</label>
                   <input
                     value={item.price}
                     onChange={(e) => handleInputChange(index, 'price', e.target.value)}
-                    className='border border-05 h-12 rounded'
+                    className={theme === "light" ? 'light4 border border-05 h-12 rounded px-6' : 'dark4 border border-03 h-12 rounded px-6'}
                   ></input>
                 </div>
-                <div>
-                  <p>Total</p>
-                  <p>{total}</p>
+                <div className='flex flex-col gap-6'>
+                  <p className={theme === "light" ? 'px13 text-07' : 'px13 text-06'}>Total</p>
+                  <p className={theme === "light" ? 'px13 text-07 mb-5' : 'px13 text-06 mb-5'}>{total}</p>
                 </div>
               </div>
               <img
@@ -170,11 +171,11 @@ const handleInputChange = (index, fieldName, value) => {
             </div>
           </div>
         ))}
-        <button onClick={() => {setItems([...items, {name: "", price: "", quantity: "", total: ""}])}} className='addnew mb-24'>+ Add New Item</button>
+        <button onClick={() => {setItems([...items, {name: "", price: "", quantity: "", total: ""}])}} className={theme === "light" ? 'addnew mb-24' : 'darkaddnew mb-24'}>+ Add New Item</button>
       </section>
-      <div className='bg-white flex h-24 w-full justify-end px-6 gap-2'>
-        <Link to={-1} className='discard'>Discard</Link>
-        <Link to="/" onClick={(e) => {dispatch(add({clientAddressCity: city2, clientAddressCountry: country2, clientAddressPostCode: postal2, clientAddressStreet: address2, clientEmail: email, clientName: name, createdAt: date, description: description, id: generateRandomId(), items: items, paymentDue: paymentDueDateString, paymentTerms: net, senderAddressCity: city, senderAddressCountry: country, senderAddressPostCode: postal, senderAddressStreet: address, status: "draft", total: total}))}} className='savedraft'>Save as Draft</Link>
+      <div className={theme === "light" ? 'light4 flex h-24 w-full items-center justify-end px-6 gap-2' : 'dark4 flex h-24 w-full items-center justify-end px-6 gap-2'}>
+        <Link to={-1} className={theme === "light" ? "discard" : "darkdiscard"}>Discard</Link>
+        <Link to="/" onClick={(e) => {dispatch(add({clientAddressCity: city2, clientAddressCountry: country2, clientAddressPostCode: postal2, clientAddressStreet: address2, clientEmail: email, clientName: name, createdAt: date, description: description, id: generateRandomId(), items: items, paymentDue: paymentDueDateString, paymentTerms: net, senderAddressCity: city, senderAddressCountry: country, senderAddressPostCode: postal, senderAddressStreet: address, status: "draft", total: total}))}} className={theme === "light" ? "savedraft" : "darksavedraft"}>Save as Draft</Link>
         <Link to="/" onClick={(e) => {dispatch(add({clientAddressCity: city2, clientAddressCountry: country2, clientAddressPostCode: postal2, clientAddressStreet: address2, clientEmail: email, clientName: name, createdAt: date, description: description, id: generateRandomId(), items: items, paymentDue: paymentDueDateString, paymentTerms: net, senderAddressCity: city, senderAddressCountry: country, senderAddressPostCode: postal, senderAddressStreet: address, status: "pending", total: total}))}} className='save'>Save & Send</Link>
       </div>
     </main>
--- src/components/ConfirmDelete.jsx
+++ src/components/ConfirmDelete.jsx
@@ -7,15 +7,16 @@ import { remove } from '../features/state/stateSlice'
 function ConfirmDelete({ ConfirmDelete, setConfirmDelete}) {
     const { id } = useParams()
     const idvalue = useSelector((state) => state.state.data[id].id)
+    const theme = useSelector((state) => state.state.toggleMode)
     const dispatch = useDispatch()
     console.log(idvalue)
   return (
-    <div className='absolute p-6 left-6 right-6 top-1/4 bg-white z-50 rounded-lg'>
-        <h1 className='mb-6 px24 text-08'>Confirm Deletion</h1>
-        <p className='px13 text-06'>Are you sure you want to delete invoice</p>
+    <div className={theme === "light" ? 'absolute p-8 left-6 right-6 top-1/4 bg-white z-50 rounded-lg' : 'absolute p-6 left-6 right-6 top-1/4 bg-03 z-50 rounded-lg'}>
+        <h1 className={theme === "light" ? 'mb-6 px24 text-08' : 'mb-6 px24 text-white'}>Confirm Deletion</h1>
+        <p className='px13 text-06 mb-2'>Are you sure you want to delete invoice</p>
         <p className='px13 text-06 mb-6'>#{idvalue}? This action cannot be undone.</p>
         <div className='flex justify-end gap-2'>
-            <button onClick={() => {setConfirmDelete(false)}} className='edit'>Cancel</button>
+            <button onClick={() => {setConfirmDelete(false)}} className={theme === "light" ? "edit" : "darkedit"}>Cancel</button>
             <Link  to="/" onClick={() => {dispatch(remove({index: id}))}} className='delete'>Delete</Link>
         </div>
     </div>
