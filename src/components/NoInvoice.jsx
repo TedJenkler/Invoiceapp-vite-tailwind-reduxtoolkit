c90793d477ab9696dae6026df2fafed9b6a57970
TedJenkler <Tedjenkler@gmail.com>
2024-03-08 16:27:41 +0100

    togglemode on no invoice

--- src/components/NoInvoice.jsx
+++ src/components/NoInvoice.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import empty from "../assets/empty.png"
+import { useSelector } from 'react-redux'
 
 function NoInvoice() {
+  const theme = useSelector((state) => state.state.toggleMode)
   return (
-    <main className='h-full bg-lightbg flex flex-col items-center pt-24'>
+    <main className={theme === "light" ? 'h-full light02 flex flex-col items-center pt-24' : 'h-full dark02 flex flex-col items-center pt-24'}>
         <img className='mb-10' src={empty} alt='empty' />
-        <h1 className='mb-6 px24 text-08'>There is nothing here</h1>
-        <p className='px13 text-06'>Create an invoice by clicking the </p>
-        <p className='px13 text-06'>New button and get started</p>
+        <h1 className={theme === "light" ? 'mb-6 px24 text-08' : 'mb-6 px24 text-white'}>There is nothing here</h1>
+        <p className={theme === "light" ? 'px13 text-06' : 'px13 text-05'}>Create an invoice by clicking the </p>
+        <p className={theme === "light" ? 'px13 text-06' : 'px13 text-05'}>New button and get started</p>
     </main>
   )
 }
