933f2d902861ff7e83b7f584a62d3f3b53b22f17
TedJenkler <Tedjenkler@gmail.com>
2024-03-12 08:35:43 +0100

    installed gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: 'localhost', // Specify the host
+    port: 3000, // Specify the port
+  },
+  base: "/Invoiceapp-vite-tailwind-reduxtoolkit/",
   plugins: [react()],
 })
